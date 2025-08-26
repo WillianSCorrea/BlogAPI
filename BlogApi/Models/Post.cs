32ded69993f123a8d5ab1fc008242ca0b1ac4334
WillianSCorrea <willianwcorrea@gmail.com>
2025-08-26 15:54:17 -0300

    Adiciona classes Post e User com validações
    Implementadas as classes `Post` e `User` nos arquivos `Post.cs` e `User.cs`. A classe `Post` inclui propriedades como `Id`, `Title`, `Content`, `CreatedAt`, `UpdatedAt`, `UserId` e uma referência ao `User`. A classe `User` agora contém `Id`, `Name`, `Email` e `PasswordHash`, com anotações de validação para garantir a integridade dos dados.

---
+++ BlogApi/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogAPI.Models;
+
+public class Post
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
---
+++ BlogApi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogApi.Models;
+public class User 
+{
+	[key]
+	public int Id { get; set; }
+    [Required]
+	[MaxLength(100)]
+	public string Name { get; set; }
+	[Required]
+    public string Email { get; set; }
+
+	public string PasswordHash { get; set; }
+
+
+
+}
